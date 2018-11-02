63d05c5dade08d05d9197c04f439e5e822042da6
root <rockavenger3011@gmail.com>
2018-11-02 15:01:23 -0400

    Starting

---
+++ codeforces/Rodolfo/1/TheatreSquareA.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+    long long a,b,c;
+    cin>>a>>b>>c;
+    long long x = ceil( (double) a/c) * ceil( (double) b/c);
+    printf("%lld\n",x);
+}
